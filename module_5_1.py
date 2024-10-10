8be63164083cc55bc03310916fc8937f1d88f957
Anatol <tapkharcity@gmail.com>
2024-10-10 14:07:38 +0800

    my homework "attributes & object's methods"

--- module_5_1.py
+++ module_5_1.py
@@ -4,16 +4,18 @@ class House:
         self.num_of_floors = num_of_floors
 
     def go_to (self, new_floor : int):
-        if new_floor <= self.num_of_floors:
+        if new_floor <= self.num_of_floors and new_floor > 0:
             i=1
             while i <= new_floor:
-                print (i)
+                print (self.name, i, 'этаж')
                 i += 1
-        else: print('Такого этажа не существует')
+        elif new_floor < 1:
+            print ( self.name, ' вниз дом не построен', new_floor)
+        else: print( self.name, ' такого этажа пока не существует', new_floor)
 
 
 h1 = House ('ЖК Горский', 18)
 h2 = House ('Домик в деревне', 2)
-
 h1.go_to(5)
-h2.go_to (2)
+h2.go_to (10)
+h1.go_to(-2)
